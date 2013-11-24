9126a172ff55e909f606146bd1fe345d0db5afb9
leopku <leopku@qq.com>
2013-11-24 10:27:11 +0800

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rt2::Application.config.secret_key_base = '093a015ec81b3cfd61e7b04e86f80e4ae94a84571ecf7adb7faf2d7897908c19f52ff085bf0d2a8d0b7a8fa9e3bffb79d9a679cae0c1d4a5b4f71c0edcd26a96'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rt2::Application.config.session_store :cookie_store, key: '_rt2_session'
